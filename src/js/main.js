02395ad54c04849ab483c1135a1ba76bbdc69a84
Quinn Briggs <briggsq@byui.edu>
2026-04-21 16:24:54 -0600

    NPS week 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
-import { getParkData } from "./parkService.mjs";
+function loadParkData() {
+  document.getElementById("parkName").textContent = "Yellowstone";
+  document.getElementById("parkType").textContent = "National Park";
+  document.getElementById("parkStates").textContent = "WY, ID, MT";
+  document.querySelector("#park-image").src = "./images/yellowstone.jpg";
+}
 
-const parkData = getParkData();
+loadParkData();
